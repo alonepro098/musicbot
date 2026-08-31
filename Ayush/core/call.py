88e7fca69b6bdcbd12e52159de445c119dff985a
alonepro098 <aloneee772@gmail.com>
2026-08-31 21:24:38 +0530

    🔧 Fix AttributeError _is_running: Remove improper PyTgCalls inheritance on Call class

--- Ayush/core/call.py
+++ Ayush/core/call.py
@@ -50,7 +50,7 @@ async def _clear_(chat_id):
     await remove_active_chat(chat_id)
 
 
-class Call(PyTgCalls):
+class Call:
     def __init__(self):
         self.userbot1 = Client(
             name="AyushAss1",
